1ab4f740cb9a789b005cf00b74fd5d8262349100
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-04-14 14:21:01 +0200

    add default prefix

--- workspaces/mkpdf/test/tutil.ts
+++ workspaces/mkpdf/test/tutil.ts
@@ -3,5 +3,5 @@ import os from "os";
 import path from "path";
 
 export function getTmpFilepath(arg: {prefix?: string, suffix?: string}): string {
-  return `${os.tmpdir()}${path.sep}${arg.prefix ?? ""}${crypto.randomUUID()}${arg.suffix ?? ""}`;
+  return `${os.tmpdir()}${path.sep}${arg.prefix ?? "mkpdf-"}${crypto.randomUUID()}${arg.suffix ?? ""}`;
 }
