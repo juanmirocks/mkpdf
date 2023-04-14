2edae33b99e86eb9e08584de4d9dd6d86846c7c5
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-04-14 17:15:51 +0200

    remove prettier and use eslint instead, also for formatting

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+  ignorePatterns: ["**/dist/"],
+  rules: {
+    quotes: ["error", "double"],
+    "brace-style": ["error", "stroustrup"],
+    "max-len": [
+      "error",
+      {
+        code: 120
+      }
+    ]
+  }
+};
